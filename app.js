7cc57d4b9ba1cd888365b4d3394a7d6bd20b5741
Christi Gilreath <130399626+christigilreath@users.noreply.github.com>
2023-05-20 19:28:30 -0400

    add javascript functionality

--- app.js
+++ app.js
@@ -1,34 +1,101 @@
 const buttons = document.querySelectorAll('button');
 const upperScreen = document.querySelector('#upper-screen');
-const lowerScreen = document.querySelector('#lower-screen');
+const lowerDisplay = document.querySelector('#lower-screen');
+const numberButtons = document.querySelectorAll("#num");
+const operateButtons = document.querySelectorAll('#op');
+const equalButton = document.querySelector("#equal");
+const clearButton = document.querySelector('#clear');
 
-let buttonClicked;
+
+let numberClicked;
+let display = '';
 
 let total = 0;
-lowerScreen.innerHTML = total;
 
-//create a function when a number is clicked it displays on the lower screen
-function handleClick (e) {
-    buttonClicked = e.target.innerHTML;
-    console.log(typeof buttonClicked)
+
+//create functions for add, subtract, multiply, divide
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply (a, b) {
+    return a * b
+}
+
+function divide (a, b) {
+    return a / b
+}
+
+function operate(num1, num2, operator) {
     
-    
-    updateLowerScreen();
-    
-
-};
-
-buttons.forEach(button => button.addEventListener('click', handleClick))
-
-
-
-function updateLowerScreen() {
-    if (buttonClicked === "Clear") {
-        lowerScreen.innerHTML = "0";
-    } else if (buttonClicked === "Delete"){
-        lowerScreen.innerHTML = '';
-
-    }else {
-        lowerScreen.innerHTML = buttonClicked;
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2)
+    } else if (operator === "*") {
+        return multiply(num1, num2)
+    } else if (operator === "/") {
+        return divide(num1, num2)
     }
 }
+
+
+//create 3 variables (first number) (operator) (second number)
+let num1;
+let operator;
+let num2;
+
+//create a function for the clicking number buttons
+function numberClick (e) {
+   numberClicked = e.target.innerHTML;
+    display +=`${numberClicked}`
+    lowerDisplay.innerHTML = display;
+    
+    
+}   
+   
+ numberButtons.forEach(number => number.addEventListener('click', numberClick))   
+    
+//creater a function for clicking operate buttons that takes the display values and stores the operator clicked
+
+function operateClick(e) {
+    operator = e.target.innerHTML;
+    num1 = parseInt(display);
+    lowerDisplay.innerHTML = '';
+    display = '';
+    
+    
+    
+}
+operateButtons.forEach(button => button.addEventListener('click', operateClick))
+
+
+//create a function for clicking equal button that
+function equalClick() {
+    num2 = parseInt(display);
+    let equalValue = operate(num1, num2, operator);
+    lowerDisplay.innerHTML = equalValue;
+    
+    
+}
+
+equalButton.addEventListener('click', equalClick)
+
+
+
+// create a function for clear button
+function clearClick () {
+num1 = '';
+num2 = '';
+operator = '';
+lowerDisplay.innerHTML ="0";
+display = '';
+}
+
+
+clearButton.addEventListener('click', clearClick)
